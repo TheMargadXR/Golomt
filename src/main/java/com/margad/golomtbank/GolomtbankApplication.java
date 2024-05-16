57a9e5f0a5143a3149730f21d6678415cb13f4f8
Margad-Erdene Kherlensukh <themargadxr@gmail.com>
2024-05-17 00:45:32 +0800

    add app

---
+++ src/main/java/com/margad/golomtbank/GolomtbankApplication.java
@@ -0,0 +1,13 @@
+package com.margad.golomtbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GolomtbankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GolomtbankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/margad/golomtbank/GolomtbankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.margad.golomtbank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GolomtbankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
